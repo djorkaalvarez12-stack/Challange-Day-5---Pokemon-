a87f5f3f51acb3c598df7cdabcaee0240daf45ab
DjorkaefAl12 <pepitoperezapache@gmail.com>
2025-09-13 09:46:51 -0500

    Actualización de los archivos index.ts y PokemonRepository.ts

--- src/repositories/PokemonRepository.ts
+++ src/repositories/PokemonRepository.ts
@@ -1,7 +1,52 @@
+import fs from 'fs';
+
 import { Pokemon } from '../models/Pokemon';
+import path from 'path';
 
 export class PokemonRepository {
+
+  private readonly FILE_PATH = 'data/pokemon-list.json';
   private pokemonList: Pokemon[] = [];
+
+  constructor() {
+    this.pokemonList = this.loadFromFile();
+  }
+
+  private loadFromFile() {
+    if(!fs.existsSync(this.FILE_PATH)){
+      return [];
+    }
+
+      try {
+      const fileContent = fs.readFileSync(this.FILE_PATH, 'utf-8');
+      const data = JSON.parse(fileContent);
+
+      const pokemons = data.map((p: any) => new Pokemon(p.name, p.level, p.stats));
+      return pokemons;
+
+    } catch (error) {
+      return [];
+    }
+
+  }
+
+  private saveToFile(){
+    const dirPath = path.dirname(this.FILE_PATH);
+
+    if (!fs.existsSync(dirPath)) {
+      fs.mkdirSync(dirPath, { recursive: true });
+    }
+
+    const pokemonArray = this.pokemonList.map(Pokemon => ({  
+      name: Pokemon.name,
+      level: Pokemon.level,
+      stats: Pokemon.stats
+
+    }));
+
+    fs.writeFileSync(this.FILE_PATH, JSON.stringify(pokemonArray, null, 2), 'utf-8');
+
+  }
 
   getPokemonList() {
     return this.pokemonList;
@@ -10,5 +55,6 @@ export class PokemonRepository {
   addPokemon(pokemon: Pokemon) {
     console.log(`Adding pokemon: ${pokemon.name}`);
     this.pokemonList.push(pokemon);
+    this.saveToFile();
   }
 }
