b7ca0fb12271d1a1445dc242b435da95407b54aa
DjorkaefAl12 <pepitoperezapache@gmail.com>
2025-09-15 19:07:17 -0500

    se agrego la clase PokeApiAdapter.ts, se actualizó index.ts y pokemon-list.json

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,14 @@
 import { Pokemon } from './models/Pokemon';
 import { PokemonRepository } from './repositories/PokemonRepository';
 import { PokemonService } from './services/PokemonService';
+import { PokeAPIAdapter } from './adapters/pokeApiAdapter';
 
-const DEFAULT_POKEMONS: Record<string, Pokemon> = {
-  bulbasaur: new Pokemon('bulbasaur', 96, { hp: 95, attack: 65, defense: 90, speed: 70 }),
-  charmander: new Pokemon('charmander', 79, { hp: 78, attack: 95, defense: 82, speed: 88 }),
-  squirtle: new Pokemon('squirtle', 64, { hp: 78, attack: 75, defense: 68, speed: 65 }),
-  pikachu: new Pokemon('pikachu', 81, { hp: 85, attack: 110, defense: 75, speed: 120 }),
-  mewtwo: new Pokemon('mewtwo', 100, { hp: 106, attack: 130, defense: 90, speed: 130 }),
-  dragonite: new Pokemon('dragonite', 88, { hp: 91, attack: 134, defense: 95, speed: 80 })
-};
 
 const pokemonRepository = new PokemonRepository();
 const pokemonService = new PokemonService(pokemonRepository);
+const pokeAPI = new PokeAPIAdapter();
 
-function main() {
+async function main() {
   const args = process.argv.slice(2);
   const command = args[0];
 
@@ -27,14 +21,23 @@ function main() {
       break;
     }
 
-    const pokemonAdd = DEFAULT_POKEMONS[pokemonName.toLowerCase()];
-    if(!pokemonAdd){
-      console.error('ESTE POKEMÓN NO ESTA EN LOS PREDETERMINADOS');
-      break;
+    try{
+      const info = await pokeAPI.getPokemonInfo(pokemonName);
+      
+      const pokemon = new Pokemon(info.name, 100, {
+        hp : info.hp,
+        attack: info.attack,
+        defense: info.defense,
+        speed: info.speed,
+      });
+
+      pokemonRepository.addPokemon(pokemon);
+      console.log("Pokemón agregado correctamente");
+    }catch(error){
+      console.log("Error al agregar el pokemón");
     }
 
-    pokemonRepository.addPokemon(pokemonAdd);
-
+    break;
    }
 
    case 'average': {
